71a73c5c567ed5f3f689d4b2baa51d61ccdbf565
Florian Weinzierl <Florian@wzma.de>
2018-02-13 00:07:56 +0100

    Imporved the test to check all default 1st level properties from streams

--- _Tests/Streams/streams.test.js
+++ _Tests/Streams/streams.test.js
@@ -3,10 +3,17 @@ var fs = require('fs')
 
 test('parse yaml delineatead by `---`', () => {
 	fs.readFile('_Streams/109-webgl-with-p5js.md', 'utf8', function(err, data){
-		var content = fm(data).attributes
+		var object = fm(data)
+
+		expect(object).toHaveProperty('body')
+
+		var content = object.attributes	
 		expect(content).toHaveProperty('title')
 		expect(content).toHaveProperty('video_number')
 		expect(content).toHaveProperty('date')
 		expect(content).toHaveProperty('video_id')
+		expect(content).toHaveProperty('topics')
+		expect(content).toHaveProperty('links')
+		expect(content).toHaveProperty('video')
 	})
 })

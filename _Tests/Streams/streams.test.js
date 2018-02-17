8559a8a572e08d928b6125f8101323dade486354
Florian Weinzierl <Florian@wzma.de>
2018-02-17 12:48:41 +0100

    Fixed Error

--- _Tests/Streams/streams.test.js
+++ _Tests/Streams/streams.test.js
@@ -4,6 +4,5 @@ getChangedFilesForRoots(['./'], {lastCommit: true,}).then(result => console.log(
 
 test('logs 1st of changedFiles', () => {
 	console.log("Hello World")
-	console.log(result.changedFiles[1])
 	return 1
 })

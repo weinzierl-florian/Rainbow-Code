034df373e4b6dde9f9e8fb8fd79ae5ee0972d423
Florian Weinzierl <Florian@wzma.de>
2018-02-17 13:28:04 +0100

    Played with tests

---
+++ _Streams/120-testingTestForStreams.md
@@ -0,0 +1,4 @@
+---
+title: this is the title
+---
+This is an Test File
--- _Tests/Streams/streams.test.js
+++ _Tests/Streams/streams.test.js
@@ -1,4 +1,8 @@
-const {getChangedFilesForRoots} = require('jest-changed-files')
+const {getChangedFilesForRoots} = require('jest-changed-files');
+
+getChangedFilesForRoots(['./'], {
+  lastCommit: true,
+}).then(result => console.log(result.changedFiles));
 
 function sum(a, b){
 	return a + b;
@@ -6,6 +10,5 @@ function sum(a, b){
 
 test('console logs all changed Files and sums 1 + 2 to equal 3', () => {
 	console.log("Hello World")
-	getChangedFilesForRoots(['./'], {lastCommit: true,}).then(result => console.log(result.changedFiles))
 	expect(sum(1, 2)).toBe(3)	
 })

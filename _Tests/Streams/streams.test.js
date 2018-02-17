204d2d25818200914a77e485ff3b7470e44e3554
Florian Weinzierl <Florian@wzma.de>
2018-02-17 12:47:09 +0100

    Added dummy test

--- _Tests/Streams/streams.test.js
+++ _Tests/Streams/streams.test.js
@@ -1,3 +1,9 @@
 const {getChangedFilesForRoots} = require('jest-changed-files')
 
 getChangedFilesForRoots(['./'], {lastCommit: true,}).then(result => console.log(result.changedFiles))
+
+test('logs 1st of changedFiles', () => {
+	console.log("Hello World")
+	console.log(result.changedFiles[1])
+	return 1
+})
